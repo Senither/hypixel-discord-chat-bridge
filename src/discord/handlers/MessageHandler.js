3c11be28e41b56fd81743e0d2d942afa727f597d
Alexis Tan <alexis@sen-dev.com>
2020-12-31 20:38:28 +0100

    Add filter to remove Discord mention strings when broadcasting messages

--- src/discord/handlers/MessageHandler.js
+++ src/discord/handlers/MessageHandler.js
@@ -6,21 +6,37 @@ class MessageHandler {
   }
 
   onMessage(message) {
-    if (! this.shouldBroadcastMessage(message)) {
+    if (!this.shouldBroadcastMessage(message)) {
       return
     }
 
     this.discord.broadcastMessage({
       username: message.member.displayName,
-      message: message.content.split('\n').join(' '),
+      message: this.stripDiscordContent(message.content),
     })
   }
 
+  stripDiscordContent(message) {
+    return message
+      .replace(/<[@|#|!|&]{1,2}(\d+){16,}>/g, '\n')
+      .replace(/<:\w+:(\d+){16,}>/g, '\n')
+      .replace(/[^\p{L}\p{N}\p{P}\p{Z}]/gu, '\n')
+      .split('\n')
+      .map(part => {
+        part = part.trim()
+
+        return part.length == 0 ? '' : part + ' '
+      })
+      .join('')
+  }
+
   shouldBroadcastMessage(message) {
-    return this.discord.client.user.id != message.author.id
-        && message.channel.id == config.discord.channel
-        && message.content
-        && message.content.length > 0
+    return (
+      this.discord.client.user.id != message.author.id &&
+      message.channel.id == config.discord.channel &&
+      message.content &&
+      message.content.length > 0
+    )
   }
 }
 
