d2f527752493b3fe51c47d6d8320472207e0a4f6
neyoa <itstomsmart@gmail.com>
2021-02-19 16:06:56 +0000

    Changed !help command to an embed

--- src/discord/commands/HelpCommand.js
+++ src/discord/commands/HelpCommand.js
@@ -1,29 +1,21 @@
-const DiscordCommand = require('../../contracts/DiscordCommand')
+const DiscordCommand = require('../../contracts/DiscordCommand');
+const Discord = require('discord.js-light')
+
+const helpEmbed = new Discord.MessageEmbed()
+.setColor('#FFFF00')
+.setTitle("Help")
+.setDescription("`< >` = Required arguments\n`[ ]` = Optional arguments")
+.addFields(
+  {name: `Command list`, value:"`!help` - Displays this command list\n`!relog [delay]` - Relogs the MC client, a delay can be given in seconds, if no delay is given it will default to 5 seconds\n`!override <command> [args]` - Executes the string attached. **This is a dangerous permission to grant**\n`!invite <player>` - Invites the specified user to the guild, providing the guild isn't full\n`!kick <user> [reason]` - Kicks the specified user from the guild\n`!promote <user>` - Promotes the specified user by 1 rank\n`!demote <user>` - Demotes the specified user by 1 rank"},
+  {name: `Shortcuts`, value: "Help: `!h`\nRelog: `!r`\nInvite: `!i`, `!inv`\nKick: `!k`\nPromote: `!p`, `!up`\nDemote: `!d`, `!down`"}
+)
+.setTimestamp()
+.setFooter('Made by Senither#0001');
 
 class HelpCommand extends DiscordCommand {
   onCommand(message) {
     message
-      .reply(
-        [
-          '`<>` = Required arguments, `[]` Optional arguments',
-          '**Command List**',
-          '`!help` - Displays this command list',
-          '`!relog [delay]` - Relogs the MC client, a delay can be given in seconds, if no delay is given it will default to 5 seconds',
-          '`!override <command> [args] - Executes the string attached. **This is a dangerous permission to grant**`',
-          "`!invite <player>` - Invites the specified user to the guild, providing the guild isn't full",
-          '`!kick <user> [reason]` - Kicks the specified user from the guild',
-          '`!promote <user>` - Promotes the specified user by 1 rank',
-          '`!demote <user>` - Demotes the specified user by 1 rank',
-          '',
-          '**Shortcuts**',
-          'Help: `!h`',
-          'Relog: `!r`',
-          'Invite: `!i`, `!inv`',
-          'Kick: `!k`',
-          'Promote: `!p`, `!up`',
-          'Demote: `!d`, `!down`',
-        ].join('\n')
-      )
+      .reply(helpEmbed)
       .then(helpMessage => setTimeout(() => helpMessage.delete(), 30000))
   }
 }
