04b3fa5467c5110ac15f092633201f33627f6701
Tom Smart <itstomsmart@gmail.com>
2021-02-20 04:10:24 +0000

    Change help command to send embedded messages (#6)
    * Changed !help command to an embed
    
    * Fix file formatting
    
    * Change embed colour to use the bots current role colour
    
    * Bump version
    
    Co-authored-by: Alexis Tan <alexis@sen-dev.com>

--- src/discord/commands/HelpCommand.js
+++ src/discord/commands/HelpCommand.js
@@ -1,30 +1,45 @@
 const DiscordCommand = require('../../contracts/DiscordCommand')
+const Discord = require('discord.js-light')
+
+const helpEmbed = new Discord.MessageEmbed()
+  .setTitle('Help')
+  .setDescription('`< >` = Required arguments\n`[ ]` = Optional arguments')
+  .addFields(
+    {
+      name: `Command list`,
+      value: [
+        '`!help` - Displays this command list',
+        '`!relog [delay]` - Relogs the MC client, a delay can be given in seconds, if no delay is given it will default to 5 seconds',
+        '`!override <command> [args]` - Executes the string attached. **This is a dangerous permission to grant**',
+        "`!invite <player>` - Invites the specified user to the guild, providing the guild isn't full",
+        '`!kick <user> [reason]` - Kicks the specified user from the guild',
+        '`!promote <user>` - Promotes the specified user by 1 rank',
+        '`!demote <user>` - Demotes the specified user by 1 rank',
+        '`!mute <user>` - Demotes the specified user by 1 rank',
+      ].join('\n'),
+    },
+    {
+      name: `Shortcuts`,
+      value: [
+        'Help: `!h`',
+        'Relog: `!r`',
+        'Override: `!o`, `!or`',
+        'Invite: `!i`, `!inv`',
+        'Kick: `!k`',
+        'Promote: `!p`, `!up`',
+        'Demote: `!d`, `!down`',
+        'Mute: `!m`',
+      ].join('\n'),
+    }
+  )
+  .setTimestamp()
+  .setFooter('Made by Senither#0001')
 
 class HelpCommand extends DiscordCommand {
   onCommand(message) {
-    message
-      .reply(
-        [
-          '`<>` = Required arguments, `[]` Optional arguments',
-          '**Command List**',
-          '`!help` - Displays this command list',
-          '`!relog [delay]` - Relogs the MC client, a delay can be given in seconds, if no delay is given it will default to 5 seconds',
-          '`!override <command> [args] - Executes the string attached. **This is a dangerous permission to grant**`',
-          "`!invite <player>` - Invites the specified user to the guild, providing the guild isn't full",
-          '`!kick <user> [reason]` - Kicks the specified user from the guild',
-          '`!promote <user>` - Promotes the specified user by 1 rank',
-          '`!demote <user>` - Demotes the specified user by 1 rank',
-          '',
-          '**Shortcuts**',
-          'Help: `!h`',
-          'Relog: `!r`',
-          'Invite: `!i`, `!inv`',
-          'Kick: `!k`',
-          'Promote: `!p`, `!up`',
-          'Demote: `!d`, `!down`',
-        ].join('\n')
-      )
-      .then(helpMessage => setTimeout(() => helpMessage.delete(), 30000))
+    message.reply(helpEmbed.setColor(message.guild.me.displayHexColor)).then(helpMessage => {
+      setTimeout(() => helpMessage.delete(), 30000)
+    })
   }
 }
 
