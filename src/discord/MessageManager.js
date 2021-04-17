d31e9be63b8cac1d0b2c192c4ff653dcfa229f2f
itsneyoa <itstomsmart@icloud.com>
2021-04-18 00:50:36 +0100

    Change messagemanager functions

--- src/discord/MessageManager.js
+++ src/discord/MessageManager.js
@@ -3,16 +3,30 @@ class MessageManager {
     this.discord = discord
   }
 
-  broadcastMessage(message, username, icon) {
+  broadcastGuildMessage(message, username, guildRank) {
     switch (this.discord.app.config.discord.messageMode.toLowerCase()) {
       case 'bot':
         this.discord.client.channels.fetch(this.discord.app.config.discord.channel).then(channel => {
-          channel.send(message)
+          channel.send({
+            embed: {
+              description: message,
+              color: '6495ED',
+              timestamp: new Date(),
+              footer: {
+                text: guildRank,
+              },
+              author: {
+                name: username,
+                icon_url: 'https://www.mc-heads.net/avatar/' + username,
+              },
+            },
+          })
         })
         break
       case 'webhook':
+        message = message.replace(/@/g, '') // Stop @everyone
         this.discord.webhook.send(
-          message, { username: username, avatarURL: icon }
+          message, { username: username, avatarURL: 'https://www.mc-heads.net/avatar/' + username }
         )
         break
       default:
@@ -20,21 +34,62 @@ class MessageManager {
     }
   }
 
-  broadcastEmbed(embed, username, icon) {
+  broadcastPlayerLogin(username) {
     switch (this.discord.app.config.discord.messageMode.toLowerCase()) {
       case 'bot':
         this.discord.client.channels.fetch(this.discord.app.config.discord.channel).then(channel => {
-          channel.send(embed)
+          channel.send({
+            embed: {
+              color: '7CFC00',
+              timestamp: new Date(),
+              author: {
+                name: `${username} joined.`,
+                icon_url: 'https://www.mc-heads.net/avatar/' + username,
+              },
+            }
+          })
         })
         break
       case 'webhook':
         this.discord.webhook.send(
-          { username: username, avatarURL: icon, embeds: [embed] }
+          { username: username, avatarURL: 'https://www.mc-heads.net/avatar/' + username, embeds: [{ color: '7CFC00', author: { name: `${username} joined.` } }] }
         )
         break
       default:
         throw new Error('Invalid message mode: must be bot or webhook')
     }
+  }
+
+  broadcastPlayerLogout(username) {
+    switch (this.discord.app.config.discord.messageMode.toLowerCase()) {
+      case 'bot':
+        this.discord.client.channels.fetch(this.discord.app.config.discord.channel).then(channel => {
+          channel.send({
+            embed: {
+              color: 'DC143C',
+              timestamp: new Date(),
+              author: {
+                name: `${username} left.`,
+                icon_url: 'https://www.mc-heads.net/avatar/' + username,
+              },
+            }
+          })
+        })
+        break
+      case 'webhook':
+        this.discord.webhook.send(
+          { username: username, avatarURL: 'https://www.mc-heads.net/avatar/' + username, embeds: [{ color: 'DC143C', author: { name: `${username} left.` } }] }
+        )
+        break
+      default:
+        throw new Error('Invalid message mode: must be bot or webhook')
+    }
+  }
+
+  broadcastInfoMessage(embed) {
+    this.discord.client.channels.fetch(this.discord.app.config.discord.channel).then(channel => {
+      channel.send({ embed: embed })
+    })
   }
 
   async setupWebhook() {
