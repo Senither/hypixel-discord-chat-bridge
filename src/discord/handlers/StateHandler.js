c3e0f228f5be98c378581d93c4b12655c283a38b
itsneyoa <itstomsmart@icloud.com>
2021-04-18 12:10:11 +0100

    Change getWebhook to a function

--- src/discord/handlers/StateHandler.js
+++ src/discord/handlers/StateHandler.js
@@ -10,7 +10,7 @@ class StateHandler {
     this.discord.client.user.setActivity('Guild Chat', { type: 'WATCHING' })
 
     if (this.discord.app.config.discord.messageMode == 'webhook') {
-      this.discord.webhook = await this.getWebhook()
+      this.discord.webhook = await getWebhook(this.discord)
     }
 
     this.discord.messageManager.broadcastInfoMessage({
@@ -18,18 +18,18 @@ class StateHandler {
       color: '7CFC00',
     }, this.discord.client.user.username, this.discord.client.user.avatarURL())
   }
+}
 
-  async getWebhook() {
-    let channel = this.discord.client.channels.cache.get(this.discord.app.config.discord.channel)
-    let webhooks = await channel.fetchWebhooks()
-    if (webhooks.first()) {
-      return webhooks.first()
-    } else {
-      var res = await channel.createWebhook(this.discord.client.user.username, {
-        avatar: this.discord.client.user.avatarURL(),
-      })
-      return res
-    }
+async function getWebhook(discord) {
+  let channel = discord.client.channels.cache.get(discord.app.config.discord.channel)
+  let webhooks = await channel.fetchWebhooks()
+  if (webhooks.first()) {
+    return webhooks.first()
+  } else {
+    var res = await channel.createWebhook(discord.client.user.username, {
+      avatar: discord.client.user.avatarURL(),
+    })
+    return res
   }
 }
 
