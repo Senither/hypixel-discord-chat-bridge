c0d1a94a03009e7ece92390722df3738881b2f3d
Joner <73760377+jonerrr@users.noreply.github.com>
2021-05-19 08:26:12 -0400

    fix prettier

--- src/discord/handlers/MessageHandler.js
+++ src/discord/handlers/MessageHandler.js
@@ -1,65 +1,58 @@
-const config = require("../../../config.json");
+const config = require('../../../config.json')
 class MessageHandler {
   constructor(discord, command) {
-    this.discord = discord;
-    this.command = command;
+    this.discord = discord
+    this.command = command
   }
 
   async onMessage(message) {
     if (!this.shouldBroadcastMessage(message)) {
-      return;
+      return
     }
 
     if (this.command.handle(message)) {
-      return;
+      return
     }
 
-    const content = this.stripDiscordContent(message.content).trim();
+    const content = this.stripDiscordContent(message.content).trim()
     if (content.length == 0) {
-      return;
+      return
     }
 
-    const isReply = await this.checkReply(message);
+    const isReply = await this.checkReply(message)
 
     this.discord.broadcastMessage({
       username: message.member.displayName,
       message: this.stripDiscordContent(message.content),
       reply: isReply,
-    });
+    })
   }
 
   async checkReply(message) {
-    if (!message.reference) return null;
+    if (!message.reference) return null
 
-    const replyingTo = await message.channel.messages.fetch(
-      message.reference.messageID
-    );
+    const replyingTo = await message.channel.messages.fetch(message.reference.messageID)
 
-    return replyingTo.author.username;
+    return replyingTo.author.username
   }
 
   stripDiscordContent(message) {
     return message
-      .replace(/<[@|#|!|&]{1,2}(\d+){16,}>/g, "\n")
-      .replace(/<:\w+:(\d+){16,}>/g, "\n")
-      .replace(/[^\p{L}\p{N}\p{P}\p{Z}]/gu, "\n")
-      .split("\n")
-      .map((part) => {
-        part = part.trim();
+      .replace(/<[@|#|!|&]{1,2}(\d+){16,}>/g, '\n')
+      .replace(/<:\w+:(\d+){16,}>/g, '\n')
+      .replace(/[^\p{L}\p{N}\p{P}\p{Z}]/gu, '\n')
+      .split('\n')
+      .map(part => {
+        part = part.trim()
 
-        return part.length == 0 ? "" : part + " ";
+        return part.length == 0 ? '' : part + ' '
       })
-      .join("");
+      .join('')
   }
 
   shouldBroadcastMessage(message) {
-    return (
-      !message.author.bot &&
-      message.channel.id == config.discord.channel &&
-      message.content &&
-      message.content.length > 0
-    );
+    return !message.author.bot && message.channel.id == config.discord.channel && message.content && message.content.length > 0
   }
 }
 
-module.exports = MessageHandler;
+module.exports = MessageHandler
--- src/minecraft/MinecraftManager.js
+++ src/minecraft/MinecraftManager.js
@@ -1,27 +1,27 @@
-const config = require("../../config.json");
-const CommunicationBridge = require("../contracts/CommunicationBridge");
-const StateHandler = require("./handlers/StateHandler");
-const ErrorHandler = require("./handlers/ErrorHandler");
-const ChatHandler = require("./handlers/ChatHandler");
-const mineflayer = require("mineflayer");
+const config = require('../../config.json')
+const CommunicationBridge = require('../contracts/CommunicationBridge')
+const StateHandler = require('./handlers/StateHandler')
+const ErrorHandler = require('./handlers/ErrorHandler')
+const ChatHandler = require('./handlers/ChatHandler')
+const mineflayer = require('mineflayer')
 
 class MinecraftManager extends CommunicationBridge {
   constructor(app) {
-    super();
+    super()
 
-    this.app = app;
+    this.app = app
 
-    this.stateHandler = new StateHandler(this);
-    this.errorHandler = new ErrorHandler(this);
-    this.chatHandler = new ChatHandler(this);
+    this.stateHandler = new StateHandler(this)
+    this.errorHandler = new ErrorHandler(this)
+    this.chatHandler = new ChatHandler(this)
   }
 
   connect() {
-    this.bot = this.createBotConnection();
+    this.bot = this.createBotConnection()
 
-    this.errorHandler.registerEvents(this.bot);
-    this.stateHandler.registerEvents(this.bot);
-    this.chatHandler.registerEvents(this.bot);
+    this.errorHandler.registerEvents(this.bot)
+    this.stateHandler.registerEvents(this.bot)
+    this.chatHandler.registerEvents(this.bot)
   }
 
   createBotConnection() {
@@ -31,19 +31,17 @@ class MinecraftManager extends CommunicationBridge {
       username: config.minecraft.username,
       password: config.minecraft.password,
       version: false,
-      auth: "mojang",
-    });
+      auth: 'mojang',
+    })
   }
 
   onBroadcast({ username, message, reply }) {
-    console.log(`Minecraft Broadcast > ${username}: ${message}`);
+    console.log(`Minecraft Broadcast > ${username}: ${message}`)
 
     if (this.bot.player !== undefined) {
-      this.bot.chat(
-        `/gc ${reply ? `Replying to: ${reply}` : `${username}:`} ${message}`
-      );
+      this.bot.chat(`/gc ${reply ? `Replying to: ${reply}` : `${username}:`} ${message}`)
     }
   }
 }
 
-module.exports = MinecraftManager;
+module.exports = MinecraftManager
